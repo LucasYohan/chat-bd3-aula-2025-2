063c1c8fd5eeb8cd146d5837b349f36506343ac1
LucasYohan <lucasyohanfmarinho@gmail.com>
2025-05-06 20:37:43 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const http = require('http');
 const socket = require('socket.io')
 const ejs = require('ejs')
 const path = require('path')
-
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,24 +17,19 @@ app.use('/', (req, res) => {
     res.render('index.html');
 })
 
-/* LOGICA DE SOCKE.IO EMVIO E PROPAGAÇÃO DE MENSAGENS*/
-
-/* ESTRUTURA DE CONEXÃO DO SOCKET.IO*/
-
 let menssagens = []
 
 io.on('connection', socket => {
+
     console.log("NOVO USUÁRIO CONECTADO: " + socket.id);
 
     socket.emit('previousMessage', menssagens);
 
     socket.on('sendMessage', data => {
         menssagens.push(data)
-
         socket.broadcast.emit('reciveMessage', data);
     })
-
-
+    
 });
 
 server.listen(3000, () => {
