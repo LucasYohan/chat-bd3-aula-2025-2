d4472441a52c387dce0da8dcb4a9bbc5eb7c1c8f
LucasYohan <lucasyohanfmarinho@gmail.com>
2025-05-06 19:43:25 -0300

    Add atualizacoes front e back

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require('express');
-
+const express = require('express')
 const http = require('http');
+const socket = require('socket.io')
+const ejs = require('ejs')
+const path = require('path')
 
 const app = express();
-
 const server = http.createServer(app);
+const io = socket(server);
 
-const ejs = require('ejs');
-const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view', path.join(__dirname, 'public'));
+app.set('view', path.join(__dirname, 'public'))
 
 app.engine('html', ejs.renderFile);
 
@@ -19,6 +19,26 @@ app.use('/', (req, res) => {
     res.render('index.html');
 })
 
+/* LOGICA DE SOCKE.IO EMVIO E PROPAGAÇÃO DE MENSAGENS*/
+
+/* ESTRUTURA DE CONEXÃO DO SOCKET.IO*/
+
+let menssagens = []
+
+io.on('connection', socket => {
+    console.log("NOVO USUÁRIO CONECTADO: " + socket.id);
+
+    socket.emit('previousMessage', menssagens);
+
+    socket.on('sendMessage', data => {
+        menssagens.push(data)
+
+        socket.broadcast.emit('reciveMessage', data);
+    })
+
+
+});
+
 server.listen(3000, () => {
-    console.log('chat rodando em - http://localhost:3000')
+    console.log('CHAT RODANDO EM - http://localhost:3000')
 });
